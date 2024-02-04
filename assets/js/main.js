d13b857db829e4647460e41e581296811bb5dd99
Bob Chrismansyah <kabangabcp20h@student.unhas.ac.id>
2024-02-05 02:21:10 +0800

    update

---
+++ assets/js/main.js
@@ -0,0 +1,208 @@
+/**
+* Template Name: ZenBlog
+* Updated: Jan 29 2024 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
+* Author: BootstrapMade.com
+* License: https:///bootstrapmade.com/license/
+*/
+
+var videoHiddenFooter = false; // Tambahkan variabel flag untuk footer
+var videoHiddenTextStart = false; // Tambahkan variabel flag untuk text-start py-4
+
+// Menggunakan JavaScript untuk mendeteksi scroll dan mengubah kelas video-post
+document.addEventListener('scroll', function() {
+  var videoPost = document.querySelector('.video-post');
+  var footer = document.querySelector('.footer');
+  var textStart = document.querySelector('.text-start');
+
+  if (!videoHiddenFooter && isInViewport(footer)) {
+    videoPost.classList.add('video-hidden');
+    videoHiddenFooter = true; // Set flag untuk footer ke true saat video tersembunyi
+  } else if (videoHiddenFooter && !isInViewport(footer)) {
+    videoHiddenFooter = false; // Set flag untuk footer ke false saat video keluar dari viewport
+  }
+
+  if (!videoHiddenTextStart && isInViewport(textStart)) {
+    videoPost.classList.add('video-hidden');
+    videoHiddenTextStart = true; // Set flag untuk text-start py-4 ke true saat video tersembunyi
+  } else if (videoHiddenTextStart && !isInViewport(textStart)) {
+    videoHiddenTextStart = false; // Set flag untuk text-start py-4 ke false saat video keluar dari viewport
+  }
+
+  // Munculkan kembali video jika tidak dalam kondisi tersembunyi dan sudah keluar dari footer atau text-start py-4
+  if (videoHiddenFooter && videoHiddenTextStart && !isInViewport(footer) && !isInViewport(textStart)) {
+    videoPost.classList.remove('video-hidden');
+    videoHiddenFooter = false;
+    videoHiddenTextStart = false;
+  }
+});
+
+// Fungsi untuk mengecek apakah elemen dalam viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+    // Fungsi untuk mengecek apakah elemen dalam viewport
+    function isInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Sticky header on scroll
+   */
+  const selectHeader = document.querySelector('#header');
+  if (selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+
+  const mobileNavToogleButton = document.querySelector('.mobile-nav-toggle');
+
+  if (mobileNavToogleButton) {
+    mobileNavToogleButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    });
+  }
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavToogleButton.classList.toggle('bi-list');
+    mobileNavToogleButton.classList.toggle('bi-x');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /**
+   * Hero Slider
+   */
+  var swiper = new Swiper(".sliderFeaturedPosts", {
+    spaceBetween: 0,
+    speed: 500,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+  });
+
+  /**
+   * Open and close the search form.
+   */
+  const searchOpen = document.querySelector('.js-search-open');
+  const searchClose = document.querySelector('.js-search-close');
+  const searchWrap = document.querySelector(".js-search-form-wrap");
+
+  searchOpen.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchWrap.classList.add("active");
+  });
+
+  searchClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchWrap.classList.remove("active");
+  });
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
